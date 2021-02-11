a1303e9cf9f0850840b8b0ff7d01139cb8979037
Pro4Soft <rodion.pronin@pro4soft.com>
2021-02-11 17:23:21 -0500

    inv sync

---
+++ Dto/P4W/ProductAvailabilityRecord.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Pro4Soft.iErpIntegration.Dto.P4W
+{
+    public class ProductAvailabilityRecord
+    {
+        public Guid ProductId { get; set; }
+        public string Sku { get; set; }
+        public string Upc { get; set; }
+        public string Category { get; set; }
+
+        public string ReferenceNumber { get; set; }
+        public string Reference1 { get; set; }
+        public string Reference2 { get; set; }
+        public string Reference3 { get; set; }
+
+        public string SubstituteGroup { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+        public Guid? ClientId { get; set; }
+        public Guid? WarehouseId { get; set; }
+        public string WarehouseCode { get; set; }
+        public decimal? OpenQuantity { get; set; }
+        public decimal? TotalQuantity { get; set; }
+        public decimal? AllocatedQuantity { get; set; }
+        public string UnitOfMeasure { get; set; }
+
+        public string BinCode { get; set; }
+        public string LicensePlateCode { get; set; }
+
+        public string GetPrintText(Func<string, string> translate)
+        {
+            var result = BinCode;
+            if (!string.IsNullOrEmpty(LicensePlateCode))
+            {
+                result = LicensePlateCode;
+                if (!string.IsNullOrEmpty(BinCode))
+                    result = $"{result}@{BinCode}";
+            }
+
+            return $"{result} - {translate($"Total [{TotalQuantity}], OnHand [{TotalQuantity - AllocatedQuantity}]")}";
+        }
+    }
+}
---
+++ Workers/Upload/InventorySyncUpload.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Pro4Soft.iErpIntegration.Dto.P4W;
+using Pro4Soft.iErpIntegration.Infrastructure;
+
+namespace Pro4Soft.iErpIntegration.Workers.Upload
+{
+    public class InventorySyncUpload : BaseWorker
+    {
+        public InventorySyncUpload(ScheduleSetting settings) : base(settings)
+        {
+
+        }
+
+        public override void Execute()
+        {
+            ExecuteAsync().Wait();
+        }
+
+        public async Task ExecuteAsync()
+        {
+            foreach (var site in App<Settings>.Instance.Sites)
+            {
+                try
+                {
+                    var time = new Stopwatch();
+                    time.Start();
+
+                    var clientId = await GetClientIdAsync(site.ClientName);
+
+                    var p4Items = await Singleton<Web>.Instance.GetInvokeAsync<List<ProductAvailabilityRecord>>($"api/ProductApi/GetAvailableInventory?hasClient=true&clientId={clientId}");
+
+                    //Do some work...
+
+                    await LogAsync($"Products quantities updated {(string.IsNullOrWhiteSpace(site.ClientName) ? "" : $"for [{site.ClientName}]")}. Elapsed time {time.Elapsed}");
+                }
+                catch (Exception ex)
+                {
+                    await LogErrorAsync(ex);
+                }
+            }
+        }
+    }
+}
